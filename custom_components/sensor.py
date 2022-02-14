aff60b2646ab9c89894351654d9b4f5160833a81
walrus416 <omarelmerhebi@gmail.com>
2022-02-14 07:39:44 -0500

    Began working on station sensor

--- custom_components/sensor.py
+++ custom_components/sensor.py
@@ -12,5 +12,17 @@ from homeassistant.components.sensor import PLATFORM_SCHEMA
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
-import homeassistant.helpers.config_validation as cv
+
+from homeassistant.components.sensor import (SensorDeviceClass, SensorEntity, SensorStateClass)
+
+_LOGGER = logging.getLogger(__name__)
+
+def setup_platform(hass: HomeAssistant,
+                   config: ConfigType,
+                   add_entities: AddEntitiesCallback,
+                   discovery_info: DiscoveryInfoType | None = None) -> None:
+    """ Setup sensor platform"""
+    add_entities([StationSensor()])
+
+class StationSensor()
 
