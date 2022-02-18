d20bc2016ab4ec3d17af633d4d2741489048cede
El Merhebi <elmerhebio2@L02228826.niaid.nih.gov>
2022-02-18 13:47:40 -0500

    some progress

--- custom_components/sensor.py
+++ custom_components/sensor.py
@@ -24,15 +24,16 @@ _LOGGER = logging.getLogger(__name__)
 
 CONF_APIKEY = 'api_key'
 CONF_TRAINS = 'trains'
-CONF_STOPID = 'stop_id'
+CONF_STOP_CODE = 'stop_code'
 CONF_LINE = 'line'
 CONF_INTERVAL = 'update_interval'
 CONF_GROUP = 'group'
 CONF_SKIP_ARR_BRD = 'skip'
 CONF_NAME = 'name'
+CONF_OFFSET = 'offset'
 
-ATTR_STOPID = 'Stop ID'
-ATTR_STOPNAME = 'Stop Name'
+ATTR_STOP_CODE = 'Stop Code'
+ATTR_STOP_NAME = 'Stop Name'
 ATTR_LINE = 'Line'
 ATTR_DESTINATION = 'Destination'
 ATTR_NEXT = 'Next Train'
@@ -44,10 +45,11 @@ UPDATE_INTERVAL = datetime.timedelta(seconds=10)
 PLATFORM_SCHEMA = SENSOR_PLATFORM_SCHEMA.extend(
     {
         vol.Required(CONF_APIKEY): cv.string,
-        vol.Optional(CONFIG_SKIP_ARR_BRD, default=FALSE): cv.boolean,
-        vol.Optional(CONFIG_NAME, default='WMATA Rail Station Sensor'): cv.string
+        vol.Optional(CONF_NAME, default='WMATA Rail Station Sensor'): cv.string,
         vol.Optional(CONF_TRAINS): [{
-            vol.Required(CONF_STOPID): cv.string,
+            vol.Required(CONF_STOP_CODE): cv.string,
+            vol.Optional(CONF_TRAIN_NUMBER, default=1): cv.integer,
+            vol.Optional(CONF_OFFSET) = cv.integer,
             vol.Required(CONF_GROUP): cv.string,
             vol.Optional(CONF_LINE): cv.string,
         }]
@@ -57,5 +59,113 @@ PLATFORM_SCHEMA = SENSOR_PLATFORM_SCHEMA.extend(
 """
 
 
+class TrainSensor(object):
+    """ The actual sensor """
+    def __init__(self, name, train_data,  group, train_number=1, offset=None, line=None):
+        """ Initialize the sensor """
+
+        self.data = train_data._trains
+
+        self._name = name
+        self._group = group
+        self._train_number = train_number
+        self._offset = offset
+        self._line = line
+
+    def update(self):
+        self.data.update()
+
+    def _get_train_data(self):
+        """ Filters train data down according to provided parameters. """
+        train_data = self.data
+
+        filtered = list(filter(lambda d: d['Group'] == self._group, train_data))
+
+        if self._offset is not None:
+            offset_list = ['BRD', 'ARR', 'DLY', '---', '']
+
+            filtered = list(filter(lambda d: d['Min'] not in offset_list, filtered))
+            filtered = list(filter(lambda d: int(d['Min']) >= self._offset, filtered))
+
+        if self._line is not None:
+            filtered = list(filter(lambda d: d['Line'] == self._line, filtered))
+
+        try:
+            filtered_final = filtered[self._train_number - 1]
+        except IndexError:
+            return None
+
+        return filtered_final
+
+    @property
+    def name(self):
+        return self.name
+
+
+class WMATAStationData(object):
+    """ Handles data retrieval to minimize API Calls"""
+    def __init__(self, api_key, station_code):
+        """ Initialize the obj """
+        self.api_key = api_key
+        self.station_code = station_code
+
+        self._headers = {
+            'api_key': api_key
+        }
+        self._update_url = f'/StationPrediction.svc/json/GetPrediction/{station_code}?%s'
+
+        self._trains = []
+
+    def update(self):
+        self._update_trains()
+
+    def _update_trains(self):
+        """ Get the train information for the station"""
+        try:
+            params = urllib.parse.urlencode({})
+            conn = http.client.HTTPSConnection('api.wmata.com')
+            conn.request('GET', self._update_url % params, '{body}', self._headers)
+            response = conn.getresponse()
+            station_data = json.loads(response.read().decode('utf-8'))
+            conn.close()
+
+        except Exception as err:
+            _LOGGER.error(f'Unable to fetch station update, got: {err}')
+
+        station_data = {k.lower(): v for k, v in station_data.items()}
+        if 'message' in station_data:
+            try:
+                status_code = station_data['statuscode']
+            except KeyError:
+                status_code = ''
+
+            error_msg = station_data['message']
+            _LOGGER.error(f'Update error, WMATA API returned {status_code}: {error_msg}')
+
+        else:
+            self._trains = station_data['trains']
+
+
 if __name__ == '__main__':
-    pass
+    station = WMATAStationData(api_key='8b9a7a21d4cb4a38925d9cc45d0d5e59', station_code='B03')
+    station._update_trains()
+
+    train = TrainSensor(name='hello', train_data=station, train_number=1, line='RD', group='1')
+    train = train._get_train_data()
+
+    print(train)
+
+    train = TrainSensor(name='hello', train_data=station, train_number=2, line='RD', group='1')
+    train = train._get_train_data()
+
+    print(train)
+
+    train = TrainSensor(name='hello', train_data=station, train_number=3, line='RD', group='1')
+    train = train._get_train_data()
+
+    print(train)
+
+    train = TrainSensor(name='hello', train_data=station, train_number=4, line='RD', group='1')
+    train = train._get_train_data()
+
+    print(train)
